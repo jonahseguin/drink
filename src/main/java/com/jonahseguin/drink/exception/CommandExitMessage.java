4d41afde27c68ec8ac7223269811746a1fff1e46
BuildTools <unconfigured@null.spigotmc.org>
2021-01-05 10:04:11 +0100

    Move printing exception into the exception itself

--- src/main/java/com/jonahseguin/drink/exception/CommandExitMessage.java
+++ src/main/java/com/jonahseguin/drink/exception/CommandExitMessage.java
@@ -1,8 +1,15 @@
 package com.jonahseguin.drink.exception;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
 public class CommandExitMessage extends Exception {
 
     public CommandExitMessage(String message) {
         super(message);
+    }
+
+    public void print(CommandSender sender) {
+        sender.sendMessage(ChatColor.RED + getMessage());
     }
 }
